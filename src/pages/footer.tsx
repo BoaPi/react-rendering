9f6610ed8983318b8cfa993751471d695a31db44
boapi <tobias.weyer1205@gmail.com>
2023-08-04 20:36:49 +0200

    add non re-render footer

--- src/pages/footer.tsx
+++ src/pages/footer.tsx
@@ -4,10 +4,10 @@ interface Footer {
   handler: () => number,
 }
 
-export default function Footer() {
+export default React.memo(function Footer() {
   console.log('Footer - render');
 
   return (
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, quibusdam.</p>
   );
-}
+})
